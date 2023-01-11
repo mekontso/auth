cf4b9601016291e15380dc46d8d5a71992452bc0
Mekontso Anderson Saa <mekontsoanderson@gmail.com>
2023-01-11 18:52:56 +0100

    Step 6 - Register endpoint

---
+++ src/main/java/com/mas/AuthController.java
@@ -0,0 +1,24 @@
+package com.mas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserRepo userRepo;
+
+    public AuthController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userRepo.save(user);
+    }
+}
