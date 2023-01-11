9569671bb00495162b0a7eb023d8ea885a54c23c
Mekontso Anderson Saa <mekontsoanderson@gmail.com>
2023-01-11 18:34:00 +0100

    Step 3 - Model create

---
+++ src/main/java/com/mas/User.java
@@ -0,0 +1,33 @@
+package com.mas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public static User of(String firstName, String lastName, String email, String password){
+        return new User(null, firstName,  lastName,  email,  password);
+    }
+
+    private User(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
